5896702777b55134a4d51878ccf128b8043b115b
Rohan Dhir <rohan.dhir@outlook.com>
2018-02-24 15:18:17 -0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="content">
+        <div className="header">
+          <h1>Flood! Made with React</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
